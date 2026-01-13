8d8f40096f96404f7686a84a7c32662f91326f19
Jawssh <joshemmanuelsalud@gmail.com>
2026-01-14 00:44:21 +0800

    add dialogue

---
+++ data/dialogue.js
@@ -0,0 +1,16 @@
+const DIALOGUE = [
+    {
+        message: "Welcome to NSHSSA Garden! Meet Ma’am Carla—the school principal of NSHSSA. She will guide you through this website."
+    },
+    {
+        message: "Welcome! We're excited to have you explore our website and learn all about the fascinating world of plants."
+    },
+    {
+        message: `Hello, ${sessionStorage.getItem("userName")}`,
+        speak: true
+    },
+    {
+        message: "Are you ready to learn more about plants?",
+        speak: true
+    }
+]
--- js/index.js
+++ js/index.js
@@ -1,15 +1,46 @@
-function checkInputLenght() {
-    const input = document.getElementById("textInput").value;
-    const button = document.getElementById("submitBtn");
+let dialogueCount = 0;
 
-    button.disabled = input.length <= 3;
-}
-function submitAndProceed() {
-    const inputValue = document.getElementById("textInput").value.trim();
+window.addEventListener("load", () => {
+    const dialogueContent = document.getElementById("dialogue-content");
+    const nextButton = document.getElementById("next-button");
+    const charImage = document.getElementById("char-image")
+    const mouthEl = document.getElementById("mouth")
+    dialogueContent.textContent = DIALOGUE[dialogueCount].message;
+    function checkInputLenght() {
+        const input = document.getElementById("textInput").value;
+        const button = document.getElementById("submitBtn");
 
-    // Save to sessionStorage
-    sessionStorage.setItem("userName", inputValue);
+        button.disabled = input.length <= 3;
+    }
+    function submitAndProceed() {
+        const inputValue = document.getElementById("textInput").value.trim();
 
-    // Redirect to next page
-    window.location.href = "html/home.html";
-}
+        // Save to sessionStorage
+        sessionStorage.setItem("userName", inputValue);
+
+
+    }
+
+    nextButton.addEventListener("click", nextDialogue)
+    function nextDialogue() {
+        if (dialogueCount + 1 < DIALOGUE.length) {
+            dialogueCount++
+            dialogueContent.textContent = DIALOGUE[dialogueCount].message;
+            if (DIALOGUE[dialogueCount].speak) {
+                charImage.src = "./assets/images/carla2.png"
+                charImage.style.marginRight = "130px"
+                mouthEl.style.visibility = "visible"
+            }
+
+        } else {
+            window.location.href = "html/home.html";
+        }
+
+
+
+    }
+
+
+
+
+});
